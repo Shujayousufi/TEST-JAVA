8369f5c9bacefa030177868847775e77ccf038e5
Shujayousufi <97071669+Shujayousufi@users.noreply.github.com>
2022-01-06 22:39:14 -0800

    Update TestJ2.java

--- src/Test/TestJ2.java
+++ src/Test/TestJ2.java
@@ -8,8 +8,16 @@ public class TestJ2 {
 		System.out.println(y/x);
 		int [] data = {4,5,66,444,2222,-33,2};
 	}
-	public static void maxValue(int [] data) {
-		
+	public static int maxValue(int [] data) {
+		return 0;
 	}
-
+	public static int minValue(int [] data) {
+		return 0;
+	}
+	public static int sumValue(int [] data) {
+		return 0;
+	}
+	public static double avgValue(int [] data) {
+		return 0;
+	}
 }
