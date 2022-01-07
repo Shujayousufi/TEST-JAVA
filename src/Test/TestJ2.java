beb907b754c3d30eb84ffb485a26068b2a1e1ce3
Shujayousufi <shujayusfi@gmail.com>
2022-01-06 22:48:06 -0800

    SO

--- src/Test/TestJ2.java
+++ src/Test/TestJ2.java
@@ -7,17 +7,38 @@ public class TestJ2 {
 		double y = 10;
 		System.out.println(y/x);
 		int [] data = {4,5,66,444,2222,-33,2};
+		System.out.println(maxValue(data));
+		System.out.println(minValue(data));
+		System.out.println(sumValue(data));
+		System.out.println(avgValue(data));
 	}
 	public static int maxValue(int [] data) {
-		return 0;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < data.length; i++) {
+			if(max < data[i]) {
+				max = data[i];
+			}
+		}
+		return max;
 	}
 	public static int minValue(int [] data) {
-		return 0;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < data.length; i++) {
+			if(min > data[i]) {
+				min = data[i];
+			}
+		}
+		return min;
 	}
 	public static int sumValue(int [] data) {
-		return 0;
+		int sum =0;
+		for (int i = 0; i < data.length; i++) {
+			sum += data[i];
+		}
+		return sum;
 	}
 	public static double avgValue(int [] data) {
-		return 0;
+		double avg = (double) sumValue(data)/ data.length;
+		return avg;
 	}
 }
