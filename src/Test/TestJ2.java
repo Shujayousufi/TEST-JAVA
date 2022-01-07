beb3ad40294cb2887fcb90f0414293ca412f855c
Shujayousufi <shujayusfi@gmail.com>
2022-01-06 22:36:13 -0800

    UP

--- src/Test/TestJ2.java
+++ src/Test/TestJ2.java
@@ -6,7 +6,10 @@ public class TestJ2 {
 		double x = 4;
 		double y = 10;
 		System.out.println(y/x);
-
+		int [] data = {4,5,66,444,2222,-33,2};
+	}
+	public static void maxValue(int [] data) {
+		
 	}
 
 }
