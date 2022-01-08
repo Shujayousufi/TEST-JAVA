62292f2872574eda7e262c468fe2cfc6fcba5ce4
Shujayousufi <shujayusfi@gmail.com>
2022-01-08 12:24:42 -0800

    change

--- src/Test/TestJ2.java
+++ src/Test/TestJ2.java
@@ -5,41 +5,68 @@ public class TestJ2 {
 	public static void main(String[] args) {
 		double x = 4;
 		double y = 10;
-		System.out.println(y/x);
-		int [] data = {4,5,66,444,2222,-33,2};
+		System.out.println(y / x);
+		int[] data = { 4, 5, 66, 444, 2222, -33, 2 };
 		System.out.println(maxValue(data));
 		System.out.println(minValue(data));
 		System.out.println(sumValue(data));
 		System.out.println(avgValue(data));
+		System.out.println("second Max: " +secondMaxValue(data));
+		System.out.println("second Min: " +secondMinValue(data));
 	}
-	public static int maxValue(int [] data) {
+
+	public static int maxValue(int[] data) {
 		int max = Integer.MIN_VALUE;
 		for (int i = 0; i < data.length; i++) {
-			if(max < data[i]) {
+			if (max < data[i]) {
 				max = data[i];
 			}
 		}
 		return max;
 	}
-	public static int minValue(int [] data) {
+
+	public static int minValue(int[] data) {
 		int min = Integer.MAX_VALUE;
 		for (int i = 0; i < data.length; i++) {
-			if(min > data[i]) {
+			if (min > data[i]) {
 				min = data[i];
 			}
 		}
 		return min;
 	}
-	public static int sumValue(int [] data) {
-		int sum =0;
+
+	public static int sumValue(int[] data) {
+		int sum = 0;
 		for (int i = 0; i < data.length; i++) {
 			sum += data[i];
 		}
 		return sum;
 	}
-	public static double avgValue(int [] data) {
-		double avg = (double) sumValue(data)/ data.length;
-		avg = Math.round(avg*100.0)/100.0;
+
+	public static double avgValue(int[] data) {
+		double avg = (double) sumValue(data) / data.length;
+		avg = Math.round(avg * 100.0) / 100.0;
 		return avg;
 	}
+	public static int secondMinValue(int[] data) {
+		int secondMin = Integer.MAX_VALUE;
+		int min = minValue(data);
+		for (int i = 0; i < data.length; i++) {
+			if (min < data[i] && secondMin > data[i]) {
+				secondMin = data[i];
+			}
+		}
+		return secondMin;
+	}
+	public static int secondMaxValue(int[] data) {
+		int secondMax = Integer.MIN_VALUE;
+		int max = maxValue(data);
+		for (int i = 0; i < data.length; i++) {
+			if (max > data[i] && secondMax < data[i]) {
+				secondMax = data[i];
+			}
+		}
+		return secondMax;
+	}
+	
 }
