f6f494634049052798c74f764c82bee1a75fb5ce
Shujayousufi <shujayusfi@gmail.com>
2022-01-09 18:48:10 -0800

    Palindrome

--- src/Test/TestJ1.java
+++ src/Test/TestJ1.java
@@ -6,7 +6,22 @@ public class TestJ1 {
 		double x = 4;
 		double y = 10;
 		System.out.println(y/x);
+		
+		String str = "DAADA";
+		Palindrome(str);
 
 	}
-
+	public static void Palindrome(String str) {
+		String rev = "";
+		for(int i = str.length()-1; i >=0; i-- ) {
+			rev += str.charAt(i);
+			
+		}
+		if(str.equals(rev)) {
+		System.out.println("the word "+ str + " is palindrome.");
+	}
+		else {
+			System.out.println("the word "+ str + " is not palindrome.");
+		}
+	}
 }
